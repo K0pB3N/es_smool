b87434f9ca8efa9c6a0e8226ae7cac01030131fb
K0pB3n <Altair1-3@yandex.ru>
2023-05-15 00:57:20 +0300

    Нерабочие тесты

---
+++ src/test/App.test.js
@@ -0,0 +1,21 @@
+
+import App from './App.vue'
+
+describe('App', () => {
+  test('renders correctly', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  test('contains a router-view component', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.findComponent({ name: 'RouterView' }).exists()).toBe(true)
+  })
+
+  test('contains a vue-page-transition component', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.findComponent({ name: 'VuePageTransition' }).exists()).toBe(
+      true
+    )
+  })
+})
